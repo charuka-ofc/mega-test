d222f72fdb5a51c089e8826dbfba092acb15f0a2
charuka-ofc <116156235+charuka-ofc@users.noreply.github.com>
2025-12-16 20:05:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -33,7 +33,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
 // [1]-------------------☟ ADD YOU SESSION ID ☟---------------------------------------------------------------------------------------------
-SESSION_ID: process.env.SESSION_ID === undefined ? 'MIZUKI-MD=cQRRkSpS#JSneSdxkgL6JzXEYQF_StX7gNi68BV2dQpKEH1l79vI' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'MIZUKI-MD=3EVzBa7Z#KWrjnOQzyBhoQYwP8i9XcU9VCACJRPOlK_APi1Vu6Tc' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER  === undefined ? '94775228949' : process.env.OWNER_NUMBER ,
 // [2]-------------------BOT PREFIX (.)-----------------------------------------------------------------------------------------------------
 PREFIX: process.env.PREFIX === undefined ? ',' : process.env.PREFIX ,
